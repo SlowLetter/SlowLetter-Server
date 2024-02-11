ab2431af64e6582513fee0c8ba29b1b26aa2686a
mingmingmon <mingmingmon@naver.com>
2024-02-11 21:46:51 +0900

    setting: 프로젝트 세팅

---
+++ src/main/java/slowLetter/spring/SlowLetterApplication.java
@@ -0,0 +1,13 @@
+package slowLetter.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowLetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlowLetterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/slowLetter/spring/SlowLetterApplicationTests.java
@@ -0,0 +1,13 @@
+package slowLetter.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlowLetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
